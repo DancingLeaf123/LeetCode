859d56665113c773646272bb8c30fba718fef43e
DancingLeaf123 <851280706@qq.com>
2022-08-23 12:28:50 +0800

    wtfc++

---
+++ Top Interview Questions/Q5.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+string s = "babad";
+
+class Solution
+{
+public:
+    string longestPalindrome(string s)
+    {
+        int i = 0, j = 0, n = s.size(), lmax = 0;
+        string p;
+        while (i < n)
+        {
+            int i1 = i, j1 = j;
+            while (j1 >= 0 && i1 < n)
+            {
+                if (s[i1++] != s[j1--])
+                    break;
+                int l = i1 - j1 - 1;
+                if (l > lmax)
+                {
+                    lmax = l;
+                    p = s.substr(j1 + 1, l);
+                }
+            }
+            if (i == j)
+                i++;
+            else
+                j++;
+        }
+        cout << p;
+        return p;
+    }
+};
+
+int main()
+{
+    cout << "Hello World!";
+    Solution mySolution;
+    mySolution.longestPalindrome(s);
+    return 0;
+}
