a8d90104f3843f57ba9dc104a9301b803080a2ee
DancingLeaf123 <851280706@qq.com>
2022-08-24 14:04:17 +0800

    ss

---
+++ Top Interview Questions/14. Longest Common Prefix.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& str) {
+        int n = str.size();
+        if(n==0) return "";
+        string ans  = "";
+        sort(begin(str), end(str));
+        string a = str[0];
+        string b = str[n-1];
+        
+        for(int i=0; i<a.size(); i++){
+            if(a[i]==b[i]){
+                ans = ans + a[i];
+            }
+            else{
+                break;
+            }
+        }
+        
+        return ans;
+        
+    }
+};
+int main() {
+  string greeting = "Hello";
+  cout << greeting;
+  return 0;
+}
