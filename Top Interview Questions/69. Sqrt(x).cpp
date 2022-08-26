8660b2bff866e6d1794a0250e6263f96f3f7d515
DancingLeaf123 <851280706@qq.com>
2022-08-26 12:30:36 +0800

    Q69 AC

--- Top Interview Questions/69. Sqrt(x).cpp
+++ Top Interview Questions/69. Sqrt(x).cpp
@@ -1,3 +1,40 @@
 #include <limits.h>
+#include <iostream>
 using namespace std;
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0) return x;
+        int left = 1, right = x/2;
+        int mid = (left + right + 1) / 2;
+        
+        while (true)
+        {
+            if (mid == (x/mid))
+            {
+                return mid;
+            }
+            if (mid < (x/mid))
+            {
+                left = mid;
+                if (mid+1 > x/(mid+1)) return mid;
+                mid = (mid + right + 1) / 2;
+            }
+            if (mid > (x/mid))
+            {
+                right = mid;
+                mid = (mid + left + 1) / 2;
+                cout << mid << endl;
+            }
+        }
+    }
+};
+
+int main(){
+    Solution mysol;
+    cout << "wtf" << endl;
+    cout << mysol.mySqrt(1024);
+    return 0;
+}
 
